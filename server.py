1f059cce1bd838ad2f42a30723b56094c9085b69
junaid-raana <jjr.dhillown@gmail.com>
2026-02-15 04:05:13 -0500

    Ran static code analysis on server.py and obtained 10/10 for PEP8 guidelines

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" This is the server module of the Emotion Detector app. """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,15 +6,18 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_detect():
+    """ This function gets the text to be analyzed.
+    Then calls the appropriate function to analyze.
+    Then based on results returns output. """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the emotion_detector function and get the response
     response = emotion_detector(text_to_analyze)
     # Error handling
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
     # Return a formatted string
-    return f"""For the given statement, the system response is 
+    return f"""For the given statement, the system response is
     'anger': {response['anger']}, 
     'disgust': {response['disgust']}, 
     'fear': {response['fear']}, 
@@ -23,6 +27,7 @@ def emotion_detect():
 
 @app.route("/")
 def render_index_page():
+    """ This function just shows the interface. """
     return render_template('index.html')
 
 if __name__ == "__main__":
